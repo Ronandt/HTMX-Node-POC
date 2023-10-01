27bfeede7c5e811c0a40deece1fc51a631e88929
Declan <declanisaackuok@gmail.com>
2023-10-01 11:12:02 +0800

    added comments

--- API/api.js
+++ API/api.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs")
 app.set("views", "./templates")
 
 
-//This is the main page
+//This is the main page. You can swap out all the elements within the body so that it can be like a new page
 app.get("/", (req, res) => {
    const data = {
       pageTitle: 'My EJS Example',
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 })
 
 
-//This page should only be for swapping out the html
+//This page should only be for swapping out the html (like a part of it is possible)
 app.get("/a", (req, res) => {
    const data = {
       pageTitle: 'Hello! This content has been swapped using HTMX. Notice that the website does not reload and no JSON is required!',
@@ -24,6 +24,7 @@ app.get("/a", (req, res) => {
   res.render('swapped', data)
 })
 
+//rednering entire page by swapping out the content of the index.ejs
 app.get("/anotherEntirePage", (req, res) => {
    res.render("newpage")
 })
